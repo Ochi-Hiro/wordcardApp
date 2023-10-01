1b5f20e092073387aa40dd484319d22ce766522c
Ochi-Hiro <120717898+shiro-kuma92@users.noreply.github.com>
2023-10-01 18:14:35 +0900

    welcome picture word

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -62,21 +62,26 @@ export default function Welcome({ auth }) {
         <div className="bg-gray-100 h-full w-full">
           <div className="pt-32 pb-24 bg-white h-full max-w-xl mx-auto">
             <div className="flex justify-center">
-              <ApplicationLogo className={'h-44'} />
+              <img src="/images/study.png" alt="本と人物の画像" className="h-48" />
             </div>
-            <p className="flex justify-center mt-4 text-gray-600 text-4xl font-bold">
+            <p className="flex justify-center mt-6 text-gray-600 text-4xl font-bold">
               シンプル単語カード
             </p>
-            <div className="border-b-2 border-gray-300 mx-24 mt-4 bg-white">
-              <p className="flex justify-center mt-12 text-gray-500 font-bold">
-                テストや資格の勉強でお馴染みの単語カードを、
-              </p>
-              <p className="flex justify-center mt-2 mb-5 text-gray-500 font-bold">
-                手軽に使えるシンプルなアプリケーションです！
-              </p>
+            <p className="flex justify-center mt-12 text-gray-500 font-bold">
+              受験や資格勉強でお馴染みの単語カードを
+            </p>
+            <p className="flex justify-center mt-2 text-gray-500 font-bold">
+              ブラウザで手軽に使えるよう再現した
+            </p>
+            <p className="flex justify-center mt-2 text-gray-500 font-bold">
+              シンプルな学習補助アプリケーションです!
+            </p>
+
+            <div className="flex justify-center mt-12 text-gray-200">
+              <p>-----------------------------------------------------------------------------</p>
             </div>
 
-            <div className="flex justify-center mt-8">
+            <div className="flex justify-center mt-12">
               <Link href={route('login')} className="mr-8">
                 <button
                   className="px-16 py-2 border-2 rounded-lg 
@@ -97,11 +102,11 @@ export default function Welcome({ auth }) {
                 </button>
               </Link>
             </div>
-            <div className="flex justify-center mt-8 text-gray-200">
+            <div className="flex justify-center mt-12 text-gray-200">
               <p>-----------------------------------------------------------------------------</p>
             </div>
-            <div className="flex justify-center mt-16">
-              <img src="/images/study.png" alt="勉強している人" className="h-40" />
+            <div className="flex justify-center mt-20">
+              <img src="/images/easy.png" alt="スマホを操作している人の画像" className="h-40" />
             </div>
             <p className="flex justify-center mt-6 text-gray-600 text-2xl font-bold">
               空き時間で手軽に使える
@@ -112,17 +117,17 @@ export default function Welcome({ auth }) {
             <p className="flex justify-center mt-2 text-gray-500 font-bold">
               いつでもどこでも手軽に使えます。
             </p>
-            <div className="flex justify-center mt-16">
-              <img src="/images/easy.png" alt="勉強している人" className="h-40" />
+            <div className="flex justify-center mt-20">
+              <img src="/images/smartphone.png" alt="スマホの画像" className="h-40" />
             </div>
             <p className="flex justify-center mt-6 text-gray-600 text-2xl font-bold">
               シンプルな操作
             </p>
             <p className="flex justify-center mt-4 text-gray-500 font-bold">
-              実際の単語カードに近づけた簡単な操作で
+              実際の単語カードに近い簡単な操作で
             </p>
             <p className="flex justify-center mt-2 text-gray-500 font-bold">
-              学習に専念することができます。
+              暗記学習に集中することができます。
             </p>
             <div className="flex justify-center mt-12 text-gray-200">
               <p>-----------------------------------------------------------------------------</p>
@@ -130,8 +135,8 @@ export default function Welcome({ auth }) {
             <p className="flex justify-center mt-8 text-gray-600 text-2xl font-bold">
               新規登録はこちらから
             </p>
-            <p className="flex justify-center mt-1 text-gray-500 font-bold">↓</p>
-            <div className="flex justify-center mt-2">
+            <p className="flex justify-center text-gray-500 font-bold">↓</p>
+            <div className="flex justify-center mt-1">
               <Link href={route('register')}>
                 <button
                   className="px-28 py-4 border-2 rounded-lg 
@@ -139,6 +144,21 @@ export default function Welcome({ auth }) {
                       bg-red-100 hover:bg-red-200"
                 >
                   <p className="text-xl font-bold text-gray-500 hover:text-gray-800">新規登録</p>
+                </button>
+              </Link>
+            </div>
+            <p className="flex justify-center mt-12 text-gray-600 text-2xl font-bold">
+              登録済みの方はこちら
+            </p>
+            <p className="flex justify-center text-gray-500 font-bold">↓</p>
+            <div className="flex justify-center mt-1">
+              <Link href={route('login')}>
+                <button
+                  className="px-28 py-4 border-2 rounded-lg 
+                      border-blue-200 hover:border-blue-300
+                      bg-blue-100 hover:bg-blue-200"
+                >
+                  <p className="text-xl font-bold text-gray-500 hover:text-gray-800">ログイン</p>
                 </button>
               </Link>
             </div>
