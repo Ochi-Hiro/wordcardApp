fd23c76ae88de0151865b386fa5b245babccfa02
Ochi-Hiro <120717898+shiro-kuma92@users.noreply.github.com>
2023-10-02 22:20:23 +0900

    indexController

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Wordcard\IndexController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -23,9 +13,8 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/index', function () {
-    return Inertia::render('Index');
-})->middleware(['auth', 'verified'])->name('index');
+Route::get('/index', [IndexController::class, 'index'])
+    ->middleware(['auth', 'verified'])->name('index');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
