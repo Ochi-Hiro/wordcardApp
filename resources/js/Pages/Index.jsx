746ba6b35358126caf7c489d40e8f405672f31de
Ochi-Hiro <120717898+shiro-kuma92@users.noreply.github.com>
2023-10-11 22:41:29 +0900

    index FontAwesomeIcon

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -1,6 +1,7 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
-import PrimaryButton from '@/Components/PrimaryButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGear, faSwatchbook, faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const Index = ({ auth, folders }) => {
   console.log(folders);
@@ -14,12 +15,30 @@ const Index = ({ auth, folders }) => {
           <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div className="max-w-5xl flex flex-wrap mx-auto px-8 py-8">
               {folders.map((folder) => (
-                <div className="w-52 h-40 mx-4 mb-6 px-4 py-3 border rounded-lg">
-                  <p className="text-lg truncate pl-1">{folder.name}</p>
+                <div className="w-52 h-44 mx-4 mb-6 px-4 pt-4 border border-gray-300 rounded-lg">
+                  <p className="text-lg font-semibold text-gray-600 truncate pl-1">{folder.name}</p>
                   <div className="mt-2">
-                    <PrimaryButton className="bg-blue-500 px-8">
-                      <p className="text-base">学習する</p>
-                    </PrimaryButton>
+                    <button className="w-full rounded-lg bg-blue-400 hover:bg-blue-300 py-3">
+                      <div className="flex justify-center text-white">
+                        <FontAwesomeIcon icon={faSwatchbook} className="pr-2 mt-1" />
+                        <p className="font-semibold">学習する</p>
+                      </div>
+                    </button>
+                  </div>
+                  <div className="flex mt-3">
+                    <div className="w-32">
+                      <button className="w-full rounded-lg bg-green-500 hover:bg-green-400 py-2">
+                        <div className="flex justify-center text-white">
+                          <FontAwesomeIcon icon={faPenToSquare} className="pr-2 mt-1" />
+                          <p className="font-semibold">編集</p>
+                        </div>
+                      </button>
+                    </div>
+                    <div className="w-10 ml-3">
+                      <button className="w-full rounded-lg bg-red-400 hover:bg-red-300 py-2">
+                        <FontAwesomeIcon icon={faTrash} className="text-white mt-1" />
+                      </button>
+                    </div>
                   </div>
                 </div>
               ))}
