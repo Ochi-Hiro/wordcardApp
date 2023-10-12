7dc79b10481d0363f15b3748f05671365c2ca9c5
Ochi-Hiro <120717898+shiro-kuma92@users.noreply.github.com>
2023-10-12 13:19:37 +0900

    index button color

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -16,7 +16,7 @@ const Index = ({ auth, folders }) => {
             <div className="max-w-5xl flex flex-wrap mx-auto px-8 py-8">
               {folders.map((folder) => (
                 <div className="w-52 h-44 mx-4 mb-6 px-4 pt-4 border border-gray-300 rounded-lg">
-                  <p className="text-lg font-semibold text-gray-600 truncate pl-1">{folder.name}</p>
+                  <p className="text-lg font-semibold text-gray-600 truncate pl-2">{folder.name}</p>
                   <div className="mt-2">
                     <button className="w-full rounded-lg bg-blue-400 hover:bg-blue-300 py-3">
                       <div className="flex justify-center text-white">
@@ -25,7 +25,7 @@ const Index = ({ auth, folders }) => {
                       </div>
                     </button>
                   </div>
-                  <div className="flex mt-3">
+                  <div className="flex mt-4">
                     <div className="w-32">
                       <button className="w-full rounded-lg bg-green-500 hover:bg-green-400 py-2">
                         <div className="flex justify-center text-white">
@@ -35,7 +35,7 @@ const Index = ({ auth, folders }) => {
                       </button>
                     </div>
                     <div className="w-10 ml-3">
-                      <button className="w-full rounded-lg bg-red-400 hover:bg-red-300 py-2">
+                      <button className="w-full rounded-lg bg-gray-400 hover:bg-gray-300 py-2">
                         <FontAwesomeIcon icon={faTrash} className="text-white mt-1" />
                       </button>
                     </div>
