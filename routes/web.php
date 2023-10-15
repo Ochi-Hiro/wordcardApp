efa634a4405ac138435570bf0c8b22688edfe5ae
Ochi-Hiro <120717898+shiro-kuma92@users.noreply.github.com>
2023-10-15 15:09:12 +0900

    FolderStore route controller, FolderCreate resource

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,10 @@ Route::prefix('folder')
 ->controller(FolderController::class)
 ->group(function(){
     Route::get('/create','create')->name('create');
+    Route::post('/store','store')->name('store');
+    Route::get('/{id}/edit','edit')->name('edit');
+    Route::post('/{id}/update','update')->name('update');
+    Route::delete('/{id}/destroy','destroy')->name('destroy');
 });
 
 Route::middleware('auth')->group(function () {
